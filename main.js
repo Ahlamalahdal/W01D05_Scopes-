bec48a8debeeb92fc5a1e1d398012f053b7ac166
Ahlamalahdal <ahlam.alahdal22@gmail.com>
2021-07-08 23:46:19 +0300

    P.C solve q1

---
+++ main.js
@@ -0,0 +1,248 @@
+console.log(arrowTitle)
+/* START CODE UNDER THIS LINE */
+
+
+/* (1)
+Q1
+
+console.log(10 >= 10); {
+
+};
+
+=> true 
+
+*/ 
+
+
+
+/*
+
+Q2
+
+console.log (5 > 3); {
+}
+
+=> true
+
+*/
+
+
+
+
+/*
+
+Q3
+ 
+console.log (5 < 10);{
+}
+else(true){
+}
+
+=> true
+
+*/
+
+
+
+
+
+/*
+Q4
+
+console.log (2 < 3 && 7 < 10); {
+
+}
+
+=> true
+
+*/
+
+
+
+
+/*
+
+Q5
+
+console.log ("hello" === "hello"){
+}
+
+=> true
+
+*/  
+
+
+
+
+
+/*(2)
+
+
+true && true && false;
+true && false
+
+=> false 
+
+
+
+*/
+
+
+
+
+/*
+
+
+true || (true && false);
+(true && false) => true 
+
+true || true 
+
+=> true
+
+*/
+
+
+
+
+
+/*
+
+5 === 2 || 1 < 10; 
+5 !== 2 || 1 < 10;
+
+=> true 
+
+
+*/
+
+
+
+
+
+/*
+ 
+(!false && true) || (!true && true);
+(true && true) || (false && true);
+ true || false 
+ 
+
+=> true 
+ 
+ */
+
+
+
+
+
+
+ /*(3)
+
+ const legalAge = function (age) {
+ return (age > 17)
+ };
+ 
+ => legalAge(20)
+true
+
+=> legalAge(10)
+false
+ 
+ */
+
+
+
+ 
+/*(4)
+
+const isItMyName = function (name){
+if (name === "ahlam"){
+    return (true);
+}else if  (name !== "ahlam"){
+    return (false)
+}
+
+
+};
+
+=> isItMyName("ahlam")
+true
+
+
+*/
+
+
+
+
+
+
+/*(5)
+
+const positiveOrNegative = function (number){
+if (number >= 21){
+    return (" is positive");
+}else if (number <= -21){
+    return ("is negative")
+}else if (number === 0){
+    return ("is neither positive nor negative")
+}
+
+};
+
+
+*/
+
+
+    
+
+
+
+
+
+//Practiice
+
+
+/*(1)
+
+const toLowerOrUpperCase = function (string , strCase){
+console.log ("string" === "strCase" )
+};
+
+
+
+  
+    
+*/
+
+const toLowerOrUpperCase = function (string , strCase)
+
+
+}
+
+
+
+/*(2)
+
+const howLong = function (string){
+if (string < 2){
+ return ("short")
+}else if (string <= 8){
+ return ("medium")
+}else if (string < 12){
+ return ("")
+
+}
+
+
+
+};
+
+
+
+
+
+
+*/
+
+
+
